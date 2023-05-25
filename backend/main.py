5ba527aed5244a74b8dee44289e078ffdc3eb1a6
aadityasinha-dotcom <aadityasinha009@gmail.com>
2023-05-25 22:05:16 +0530

    reformated the code using black/flake

--- backend/main.py
+++ backend/main.py
@@ -10,17 +10,17 @@ import requests
 import networkx as nx
 
 
-
 class RouteRequest(BaseModel):
     DeparturePosition: List[float]
     DestinationPosition: List[float]
     DepartureTime: str = datetime.now().isoformat()
 
+
 OVERPASS_URL = "http://overpass-api.de/api/interpreter"
 
 
 app = FastAPI()
-client = boto3.client('location', region_name='ap-south-1')
+client = boto3.client("location", region_name="ap-south-1")
 
 
 """
@@ -44,9 +44,10 @@ def coord_to_id(coord):
     node(around:20, {coord[1]}, {coord[0]});
     out;
     """
-    response = requests.get(OVERPASS_URL, params={'data': overpass_query})
+    response = requests.get(OVERPASS_URL, params={"data": overpass_query})
     id_ = response.json()["elements"][0]["id"]
     return id_
+
 
 def id_to_coord(id_):
     overpass_query = f"""
@@ -54,21 +55,19 @@ def id_to_coord(id_):
     node({id_});
     out;
     """
-    response = requests.get(OVERPASS_URL, params={'data': overpass_query})
+    response = requests.get(OVERPASS_URL, params={"data": overpass_query})
     lat = response.json()["elements"][0]["lat"]
     lon = response.json()["elements"][0]["lon"]
-    return [lat, lon] 
+    return [lat, lon]
 
 
 @app.get("/api/search/")
-async def search(text: str, maxResults: int=5):
+async def search(text: str, maxResults: int = 5):
     """
     Autocompletion Support
     """
     response = client.search_place_index_for_text(
-        IndexName="GypsyPlaceIndex",
-        Text=text,
-        MaxResults=maxResults
+        IndexName="GypsyPlaceIndex", Text=text, MaxResults=maxResults
     )
 
     return response
@@ -76,6 +75,7 @@ async def search(text: str, maxResults: int=5):
     # uncomment for using openstreetmaps api
     # response = requests.get(f"https://nominatim.openstreetmap.org/search.php?q={text}&format=jsonv2&limit={maxResults}")
     # return json.loads(response.content)
+
 
 @app.post("/api/getRoute/")
 async def getRoute(route: RouteRequest):
@@ -88,10 +88,10 @@ async def getRoute(route: RouteRequest):
         f"{route.DestinationPosition[0]},{route.DestinationPosition[1]}"
         f"?geometries=geojson&alternatives=true&overview=full"
     )
-    
+
     routes = json.loads(response.content)["routes"]
     # print(routes)
-    
+
     listWithDistDurAndRoutes = []
     res = []
     for route in routes:
@@ -100,9 +100,9 @@ async def getRoute(route: RouteRequest):
             l[:] = reversed(l[:])
         res.append(ls)
         Place = {}
-        Place['distance'] = route['legs'][0]['distance']
-        Place['duration'] =  route['legs'][0]['duration']
-        Place['route'] = ls
+        Place["distance"] = route["legs"][0]["distance"]
+        Place["duration"] = route["legs"][0]["duration"]
+        Place["route"] = ls
 
         listWithDistDurAndRoutes.append(Place)
 
@@ -116,10 +116,10 @@ async def getRoutes(route: RouteRequest):
     """
     todays_date = datetime.today() + timedelta(days=1)
     times = [
-        time(hour=1, minute=00, second=00), 
-        time(hour=9, minute=00, second=00), 
+        time(hour=1, minute=00, second=00),
+        time(hour=9, minute=00, second=00),
         time(hour=12, minute=00, second=00),
-        time(hour=17, minute=15, second=00) 
+        time(hour=17, minute=15, second=00),
     ]
 
     diff_times = [datetime.combine(todays_date, time_) for time_ in times]
@@ -128,17 +128,15 @@ async def getRoutes(route: RouteRequest):
     for time_ in diff_times:
         route.DepartureTime = time_.isoformat()
         response = client.calculate_route(
-            CalculatorName='GypsyRouteCalculator',
+            CalculatorName="GypsyRouteCalculator",
             DepartureTime=route.DepartureTime,
             DeparturePosition=route.DeparturePosition,
-            DestinationPosition=route.DestinationPosition
+            DestinationPosition=route.DestinationPosition,
         )
 
         responses.append(response)
-    
-    return {
-        "data": responses
-    }
+
+    return {"data": responses}
 
 
 @app.post("/api/getSafestPath/")
@@ -147,19 +145,24 @@ async def getSafestPath(route: RouteRequest):
     Get Safest Path
     """
     response = requests.get(
-            f"http://router.project-osrm.org/route/v1/car/"
-            f"{route.DeparturePosition[0]},{route.DeparturePosition[1]};"
-            f"{route.DestinationPosition[0]},{route.DestinationPosition[1]}"
-            f"?geometries=geojson&alternatives=true&overview=full"
-        )
+        f"http://router.project-osrm.org/route/v1/car/"
+        f"{route.DeparturePosition[0]},{route.DeparturePosition[1]};"
+        f"{route.DestinationPosition[0]},{route.DestinationPosition[1]}"
+        f"?geometries=geojson&alternatives=true&overview=full"
+    )
 
     routes = json.loads(response.content)["routes"]
     optimal_path = routes[0]["geometry"]["coordinates"]
 
     maxLon, maxLat, minLon, minLat = -181, -91, 181, 91
     for long, lat in optimal_path:
-        maxLon, maxLat, minLon, minLat = max(maxLon, long),max(maxLat, lat),min(minLon, long),min(minLat, lat)
-    
+        maxLon, maxLat, minLon, minLat = (
+            max(maxLon, long),
+            max(maxLat, lat),
+            min(minLon, long),
+            min(minLat, lat),
+        )
+
     # creating bbox
     south, north, west, east = minLat, maxLat, minLon, maxLon
     margin = 0.1
@@ -167,7 +170,7 @@ async def getSafestPath(route: RouteRequest):
     lon_margin = margin * (maxLon - minLon)
 
     bbox = f"{south-lat_margin},{west-lon_margin},{north+lat_margin},{east-lon_margin}"
-    
+
     # creating overpass query
     overpass_url = "http://overpass-api.de/api/interpreter"
     overpass_query = f"""
@@ -188,19 +191,17 @@ async def getSafestPath(route: RouteRequest):
             G.add_node(element["id"], lat=element["lat"], lon=element["lon"])
         if element["type"] == "way":
             nodes = element["nodes"]
-            for i in range(len(nodes)-1):
-                G.add_edge(nodes[i], nodes[i+1])
-        
-
+            for i in range(len(nodes) - 1):
+                G.add_edge(nodes[i], nodes[i + 1])
 
     # getting source and destination id
     src_id = coord_to_id(route.DeparturePosition)
     dest_id = coord_to_id(route.DestinationPosition)
-    
+
     # assigning random weights
     for u, v in G.edges:
-        G.edges[u,v]['weight'] = random.randint(1, 10)
-    
+        G.edges[u, v]["weight"] = random.randint(1, 10)
+
     try:
         safe_path = nx.dijkstra_path(G, src_id, dest_id)
         safe_path_coord = []
@@ -214,9 +215,11 @@ async def getSafestPath(route: RouteRequest):
             l[:] = reversed(l[:])
         final_path = optimal_path
 
-    bounding_box = [[minLat,minLon],[minLat,maxLon],[maxLat,minLon],[maxLat, maxLon]]
+    bounding_box = [
+        [minLat, minLon],
+        [minLat, maxLon],
+        [maxLat, minLon],
+        [maxLat, maxLon],
+    ]
 
-    return {
-        "final_path": final_path,
-        "bounding_box": bounding_box
-    }
+    return {"final_path": final_path, "bounding_box": bounding_box}
